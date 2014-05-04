098c18a2b1eb303cdc78d3be0479a19842688169
suntinghui <tinghuisun@163.com>
2014-05-04 21:03:20 +0800

    STH

---
+++ Common/AES.h
@@ -0,0 +1,13 @@
+//
+//  AES.h
+//  POS4iPhone_PeoplePay
+//
+//  Created by  STH on 4/29/14.
+//  Copyright (c) 2014 文彬. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AES : NSObject
+
+@end
